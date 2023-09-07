9c91615a24cbb36ae9ab576da368c35ae75dcce7
Bouchra Madkour <madkourbouchra@gmail.com>
2023-09-07 15:02:41 +0200

    first version of the form challenge

---
+++ form.js
@@ -0,0 +1,36 @@
+const submit = document.querySelector("button");
+const name = document.querySelector(".nameInput");
+const email = document.querySelector(".emailInput");
+const phoneNumber = document.querySelector(".phoneNumberInput");
+const spanName = document.querySelector(".spanName");
+const spanEmail = document.querySelector(".spanEmail");
+const spanPhoneNumber = document.querySelector(".spanPhoneNumber");
+
+const moroccanPhoneNumberPattern = /^(05|06|07)[0-9]{8}$/;
+const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+const divHTML = `<div><p style="color: red">wrong !!!!!</p></div>`;
+
+spanName.style.display = "none";
+spanEmail.style.display = "none";
+spanPhoneNumber.style.display = "none";
+
+submit.addEventListener("click", () => {
+  const testingEmail = emailPattern.test(email.value);
+  if (name.value.length < 2) {
+    spanName.style.display = "inline";
+  } else {
+    spanName.style.display = "none";
+  }
+  if (!testingEmail) {
+    spanEmail.style.display = "inline";
+  } else {
+    spanEmail.style.display = "none";
+  }
+  const testingNumber = moroccanPhoneNumberPattern.test(phoneNumber.value);
+  if (!testingNumber) {
+    spanPhoneNumber.style.display = "inline";
+  } else {
+    spanPhoneNumber.style.display = "none";
+  }
+});
